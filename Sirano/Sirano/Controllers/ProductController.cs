d77f06f0b9dfc5752b5004b316fb5ecaa7aecea6
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-06-11 12:27:54 +0200

    Added sorting

--- Sirano/Sirano/Controllers/ProductController.cs
+++ Sirano/Sirano/Controllers/ProductController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Newtonsoft.Json.Linq;
 using Sirano.Data;
 using Sirano.Models;
 
@@ -13,6 +15,7 @@ namespace Sirano.Controllers
     public class ProductController : Controller
     {
         private readonly ApplicationDbContext _context;
+        public static Boolean men;
 
         public ProductController(ApplicationDbContext context)
         {
@@ -20,26 +23,31 @@ namespace Sirano.Controllers
         }
 
         // GET: Products
-        public async Task<IActionResult> Index(bool isMen, string productType)
+        public async Task<IActionResult> Index(bool isMen)
         {
             var products = _context.Product;
+            men = isMen;
             var filteredProducts = products.Where(p => p.Men == isMen);
 
+            return View(await filteredProducts.ToListAsync());
+        }
+        public async Task<IActionResult> Index1(string productType)
+        {
+            var filteredProducts = _context.Product.Where(p => p.Men == men);
             if (!string.IsNullOrEmpty(productType))
             {
                 switch (productType.ToLower())
                 {
                     case "footwear":
-                        filteredProducts = filteredProducts.Where(p => p is Footwear && p.Men == isMen);
+                        filteredProducts = filteredProducts.Where(p => p is Footwear && p.Men == men);
                         break;
                     case "clothing":
-                        filteredProducts = filteredProducts.Where(p => p is Clothing && p.Men == isMen);
+                        filteredProducts = filteredProducts.Where(p => p is Clothing && p.Men == men);
                         break;
                 }
             }
-            return View(await filteredProducts.ToListAsync());
+            return View("Index", await filteredProducts.ToListAsync());
         }
-
         // GET: Product/Details/5
         public async Task<IActionResult> Details(int? id)
         {
@@ -58,7 +66,7 @@ namespace Sirano.Controllers
             return View(product);
         }
 
-       
+
         private bool ProductExists(int id)
         {
             return _context.Product.Any(e => e.Id == id);
